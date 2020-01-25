a48f37d81b58ca4cd8fd778696ae22dc3effee6b
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-01-25 16:34:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/treasurehunt/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,69 @@
+package com.example.treasurehunt
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.treasurehunt.HuntMainActivity.Companion.ACTION_GEOFENCE_EVENT
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+
+/*
+ * Triggered by the Geofence.  Since we only have one active Geofence at once, we pull the request
+ * ID from the first Geofence, and locate it within the registered landmark data in our
+ * GeofencingConstants within GeofenceUtils, which is a linear string search. If we had  very large
+ * numbers of Geofence possibilities, it might make sense to use a different data structure.  We
+ * then pass the Geofence index into the notification, which allows us to have a custom "found"
+ * message associated with each Geofence.
+ */
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == ACTION_GEOFENCE_EVENT) {
+            val geofencingEvent = GeofencingEvent.fromIntent(intent)
+
+            if (geofencingEvent.hasError()) {
+                val errorMessage = errorMessage(context, geofencingEvent.errorCode)
+                Log.e(TAG, errorMessage)
+                return
+            }
+
+            if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                Log.v(TAG, context.getString(R.string.geofence_entered))
+
+                val fenceId = when {
+                    geofencingEvent.triggeringGeofences.isNotEmpty() ->
+                        geofencingEvent.triggeringGeofences[0].requestId
+                    else -> {
+                        Log.e(TAG, "No Geofence Trigger Found! Abort mission!")
+                        return
+                    }
+                }
+                // Check geofence against the constants listed in GeofenceUtil.kt to see if the
+                // user has entered any of the locations we track for geofences.
+                val foundIndex = GeofencingConstants.LANDMARK_DATA.indexOfFirst {
+                    it.id == fenceId
+                }
+
+                // Unknown Geofences aren't helpful to us
+                if ( -1 == foundIndex ) {
+                    Log.e(TAG, "Unknown Geofence: Abort Mission")
+                    return
+                }
+
+                val notificationManager = ContextCompat.getSystemService(
+                    context,
+                    NotificationManager::class.java
+                ) as NotificationManager
+
+                notificationManager.sendGeofenceEnteredNotification(
+                    context, foundIndex
+                )
+            }
+        }
+    }
+}
+
+private const val TAG = "GeofenceReceiver"
---
+++ app/src/main/java/com/example/treasurehunt/GeofenceViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.treasurehunt
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+/*
+ * This class contains the state of the game.  The two important pieces of state are the index
+ * of the geofence, which is the geofence that the game thinks is active, and the state of the
+ * hint being shown.  If the hint matches the geofence, then the Activity won't update the geofence
+ * as it cycles through various activity states.
+ *
+ * These states are stored in SavedState, which matches the Android lifecycle.  Destroying the
+ * associated Activity with the back action will delete all state and reset the game, while
+ * the Home action will cause the state to be saved, even if the game is terminated by Android in
+ * the background.
+ */
+class GeofenceViewModel(state: SavedStateHandle) : ViewModel() {
+    private val _geofenceIndex = state.getLiveData(GEOFENCE_INDEX_KEY, -1)
+    private val _hintIndex = state.getLiveData(HINT_INDEX_KEY, 0)
+    val geofenceIndex: LiveData<Int>
+        get() = _geofenceIndex
+
+    val geofenceHintResourceId = Transformations.map(geofenceIndex) {
+        val index = geofenceIndex.value ?: -1
+        when {
+            index < 0 -> R.string.not_started_hint
+            index < GeofencingConstants.NUM_LANDMARKS -> GeofencingConstants.LANDMARK_DATA[geofenceIndex.value!!].hint
+            else -> R.string.geofence_over
+        }
+    }
+
+    val geofenceImageResourceId = Transformations.map(geofenceIndex) {
+        val index = geofenceIndex.value ?: -1
+        when {
+            index < GeofencingConstants.NUM_LANDMARKS -> R.drawable.android_map
+            else -> R.drawable.android_treasure
+        }
+    }
+
+    fun updateHint(currentIndex: Int) {
+        _hintIndex.value = currentIndex+1
+    }
+
+    fun geofenceActivated() {
+        _geofenceIndex.value = _hintIndex.value
+    }
+
+    fun geofenceIsActive() =_geofenceIndex.value == _hintIndex.value
+    fun nextGeofenceIndex() = _hintIndex.value ?: 0
+}
+
+private const val HINT_INDEX_KEY = "hintIndex"
+private const val GEOFENCE_INDEX_KEY = "geofenceIndex"
